238e86650354f5e82b9913eb28a57ba906e07c3d
안정현 <anjeonghyeon@anjeonghyeon-ui-MacBookPro.local>
2026-07-14 14:40:59 +0900

    feat: 폴더 구조 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MedicationsModule } from './medications/medications.module';
+import { RecordsModule } from './records/records.module';
+import { FamilyModule } from './family/family.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, MedicationsModule, RecordsModule, FamilyModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
