f613be9c55143ad0999f41a934e2d748529c5616
0lin30 <yujeong430@gmail.com>
2026-07-18 02:25:32 +0900

    feat: TypeORM 연결 및 USer 엔티티 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule], // 다른 모듈(auth 등)에서도 User 리포지토리 쓸 수 있게
+})
 export class UsersModule {}
