e2462fec05d1c9bf66dbf93e6abeb8ac8d5396c6
yujig12 <yujin99992@gmail.com>
2026-08-04 14:59:25 +0900

    feat: 약 CRUD DB 연동

--- src/medications/dto/update-medication.dto.ts
+++ src/medications/dto/update-medication.dto.ts
@@ -1,6 +1,19 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class UpdateMedicationDto {
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
+  @IsString()
   dosage?: string;
+
+  @IsOptional()
+  @IsString()
   time?: string;
+
+  @IsOptional()
+  @IsString()
   memo?: string;
 }
